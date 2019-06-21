df53497fb0905122d71772e2cefd2b65a86dca82
tevez07b9 <talmeezahmed786@gmail.com>
2019-06-21 18:08:37 +0530

    Adding Files

---
+++ index.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+app.get("/user", (req, res) => {
+  res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
+});
+
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
